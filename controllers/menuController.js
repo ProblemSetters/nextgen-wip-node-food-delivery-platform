89f291c9bb73b8787dcb187380accc51f77ca088
sahilphulwani <sahil.phulwani@problemsetters.com>
2025-05-07 15:07:15 +0530

    Feat: boiler-plate code

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -10,33 +10,7 @@ const readRestaurants = () => {
 const writeRestaurants = (restaurants) => {
   fs.writeFileSync(writePath, JSON.stringify(restaurants, null, 2));
 };
+  
+exports.addMenuItem = async (req, res) => {
 
-
-const addItem = async (restaurantId, menuItem) => {
-    let restaurants = await readRestaurants();
-    const restaurantIndex = restaurants.findIndex((r) => r.id === restaurantId);
-  
-    if (restaurantIndex === -1) {
-      const error = new Error(`Restaurant with ID ${restaurantId} not found.`);
-      error.status = 404;
-      throw error;
-    }
-  
-    restaurants[restaurantIndex].menu.push(menuItem);
-    await writeRestaurants(restaurants);
-  
-    return restaurants[restaurantIndex].menu;
-  };
-  
-  exports.addMenuItem = async (req, res) => {
-    const { restaurantId } = req.params;
-    const newItem = req.body;
-  
-    const result = await addItem(restaurantId, newItem);
-    if (result.error) {
-      return res.status(400).json({ error: result.error });
-    }
-    res
-      .status(201)
-      .json({ message: "Menu item added successfully", menu: result });
-  };
+};
--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -6,33 +6,5 @@ const restaurants = JSON.parse(
 );
 
 exports.getData = (req, res) => {
-    const { cuisine, location, rating, availability } = req.query;
-
-  let filteredRestaurants = restaurants;
-
-  if (cuisine) {
-    filteredRestaurants = filteredRestaurants.filter((restaurant) =>
-      restaurant.cuisine.some((c) => c.toLowerCase() === cuisine.toLowerCase())
-    );
-  }
-  if (location) {
-    filteredRestaurants = filteredRestaurants.filter(
-      (restaurant) =>
-        restaurant.location.toLowerCase() === location.toLowerCase()
-    );
-  }
-  if (rating) {
-    const minRating = parseFloat(rating);
-    filteredRestaurants = filteredRestaurants.filter(
-      (restaurant) => restaurant.rating >= minRating
-    );
-  }
-  if (availability !== undefined) {
-    const isAvailable = availability === "true";
-    filteredRestaurants = filteredRestaurants.filter(
-      (restaurant) => restaurant.availability === isAvailable
-    );
-  }
-
-  res.status(200).json(filteredRestaurants);
+    
 };
