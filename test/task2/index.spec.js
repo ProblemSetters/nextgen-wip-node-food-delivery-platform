46e3cfbfbb867351b20e240b5a72d474aac5b882
sahilphulwani <sahil.phulwani@problemsetters.com>
2025-05-09 22:37:48 +0530

    Update: task2 config

--- test/task2/index.spec.js
+++ test/task2/index.spec.js
@@ -7,17 +7,6 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe("Sample Test Cases", () => {
-  // Passing test case
-  it("should return 200 for the root endpoint", (done) => {
-    chai
-      .request(app)
-      .get("/") 
-      .end((err, res) => {
-        res.should.have.status(200); 
-        done();
-      });
-  });
-
   // Failing test case
   it("should return 200 for a non-existent endpoint", (done) => {
     chai
